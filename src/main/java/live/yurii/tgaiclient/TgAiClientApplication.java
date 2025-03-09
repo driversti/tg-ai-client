2b9d884e409bee2125a7895c5acd9eb021c782fa
driversti <github@seniorjava.dev>
2025-03-09 11:57:22 +0100

    initial commit

---
+++ src/main/java/live/yurii/tgaiclient/TgAiClientApplication.java
@@ -0,0 +1,13 @@
+package live.yurii.tgaiclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgAiClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TgAiClientApplication.class, args);
+  }
+
+}
---
+++ src/test/java/live/yurii/tgaiclient/TgAiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package live.yurii.tgaiclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgAiClientApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
