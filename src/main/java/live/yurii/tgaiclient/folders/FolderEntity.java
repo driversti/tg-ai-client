5786dac1a36e3bcf61ea398e9e570d4694e8ed2d
driversti <github@seniorjava.dev>
2025-03-31 09:07:00 +0200

    removed folder items

--- src/main/java/live/yurii/tgaiclient/folders/FolderEntity.java
+++ src/main/java/live/yurii/tgaiclient/folders/FolderEntity.java
@@ -1,20 +1,15 @@
 package live.yurii.tgaiclient.folders;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 @Getter
 @Setter
@@ -30,27 +25,9 @@ public class FolderEntity {
   @Column(name = "name", nullable = false)
   private String name;
 
-  @OneToMany(
-      mappedBy = "folder",
-      cascade = CascadeType.ALL, // Cascade persist, merge, remove etc. to FolderItems
-      orphanRemoval = true, // Delete FolderItems removed from this collection
-      fetch = FetchType.LAZY
-  )
-  private Set<FolderItemEntity> folderItems = new HashSet<>();
-
   public FolderEntity(Integer id, String name) {
     this.id = id;
     this.name = name;
-  }
-
-  public void addItem(FolderItemEntity folderItem) {
-    folderItems.add(folderItem);
-    folderItem.setFolder(this);
-  }
-
-  public void removeItem(FolderItemEntity folderItem) {
-    folderItems.remove(folderItem);
-    folderItem.setFolder(null);
   }
 
   @Override
--- src/main/java/live/yurii/tgaiclient/folders/FolderEventListener.java
+++ src/main/java/live/yurii/tgaiclient/folders/FolderEventListener.java
@@ -1,6 +1,6 @@
 package live.yurii.tgaiclient.folders;
 
-import live.yurii.tgaiclient.chats.UpdateNewChatEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.drinkless.tdlib.TdApi;
 import org.springframework.context.event.EventListener;
@@ -9,37 +9,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Collectors;
-
-import static live.yurii.tgaiclient.utils.JsonUtil.toJson;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class FolderEventListener {
-
-  private static final int LOCK_STRIPE_COUNT = 64;
-  private final Lock[] chatUpdateLocks = new Lock[LOCK_STRIPE_COUNT];
 
   private final FolderRepository folderRepository;
   private final FolderMapper folderMapper;
-  private final FolderItemRepository folderItemRepository;
-  private final FolderItemsSyncService folderItemsSyncService;
-
-  public FolderEventListener(FolderRepository folderRepository, FolderMapper folderMapper,
-                             FolderItemRepository folderItemRepository, FolderItemsSyncService folderItemsSyncService) {
-    this.folderRepository = folderRepository;
-    this.folderMapper = folderMapper;
-    this.folderItemRepository = folderItemRepository;
-    this.folderItemsSyncService = folderItemsSyncService;
-    for (int i = 0; i < LOCK_STRIPE_COUNT; i++) {
-      chatUpdateLocks[i] = new ReentrantLock();
-    }
-  }
 
   @Transactional
   @EventListener
@@ -69,80 +49,5 @@ public class FolderEventListener {
     folderRepository.saveAll(foldersToKeep);
     folderRepository.deleteAll(foldersToDelete);
     log.trace("Updated chat folders: {}, deleted: {}", foldersToKeep.size(), foldersToDelete.size());
-  }
-
-  @Transactional
-  @EventListener
-  public void onUpdateChatAddedToFolderListEvent(UpdateChatAddedToListEvent event) {
-    TdApi.UpdateChatAddedToList update = event.getUpdate();
-    if (update == null || update.getConstructor() != TdApi.UpdateChatAddedToList.CONSTRUCTOR ||
-        update.chatList.getConstructor() != TdApi.ChatListFolder.CONSTRUCTOR) {
-      return;
-    }
-
-    int folderId = ((TdApi.ChatListFolder) update.chatList).chatFolderId;
-    FolderItemEntity.FolderItemId itemId = new FolderItemEntity.FolderItemId(folderId, update.chatId);
-    folderItemRepository.findById(itemId)
-        .ifPresentOrElse(e -> {
-          log.debug("{} already added", update.chatId);
-        }, () -> saveNewItem(folderId, itemId));
-  }
-
-  @Transactional
-  @EventListener
-  public void onUpdateChatRemovedFromFolderListEvent(UpdateChatRemovedFromListEvent event) {
-    TdApi.UpdateChatRemovedFromList update = event.getUpdate();
-    if (update == null || update.getConstructor() != TdApi.UpdateChatRemovedFromList.CONSTRUCTOR ||
-        update.chatList.getConstructor() != TdApi.ChatListFolder.CONSTRUCTOR) {
-      return;
-    }
-    TdApi.ChatListFolder listFolder = (TdApi.ChatListFolder) update.chatList;
-    FolderItemEntity.FolderItemId itemId = new FolderItemEntity.FolderItemId(listFolder.chatFolderId, update.chatId);
-    folderItemRepository.deleteById(itemId);
-    log.debug("{} removed from folder {}", update.chatId, listFolder.chatFolderId);
-  }
-
-  //@EventListener it seems to be not working as I expected
-  public void onUpdateNewChatEvent(UpdateNewChatEvent event) {
-    TdApi.UpdateNewChat update = event.getUpdate();
-    if (update == null || update.getConstructor() != TdApi.UpdateNewChat.CONSTRUCTOR) {
-      return;
-    }
-    TdApi.Chat chat = update.chat;
-    TdApi.ChatList[] chatLists = chat.chatLists;
-    Set<Integer> currentFolderIdsFromEvent = extractCurrentFolderIds(chatLists);
-
-    long chatId = chat.id;
-    Lock lock = getLockForChatId(chatId);
-    lock.lock();
-    try {
-      log.debug("Lock acquired for chatId {}", chatId);
-      folderItemsSyncService.performDbSyncInTransaction(chatId, currentFolderIdsFromEvent);
-    } finally {
-      lock.unlock();
-      log.debug("Lock released for chatId {}", chatId);
-    }
-  }
-
-  private void saveNewItem(int folderId, FolderItemEntity.FolderItemId itemId) {
-    folderRepository.findById(folderId)
-        .ifPresent(folderEntity -> {
-          FolderItemEntity itemEntity = new FolderItemEntity(itemId, folderEntity);
-          folderItemRepository.save(itemEntity);
-          log.debug("{} added to folder {}", itemId.getItemId(), folderEntity.getName());
-        });
-  }
-
-  private Lock getLockForChatId(long chatId) {
-    int lockIndex = Math.abs(Long.hashCode(chatId)) % LOCK_STRIPE_COUNT;
-    return chatUpdateLocks[lockIndex];
-  }
-
-  private static Set<Integer> extractCurrentFolderIds(TdApi.ChatList[] chatLists) {
-    return (chatLists == null) ? new HashSet<>() :
-        Arrays.stream(chatLists)
-            .filter(folder -> folder.getConstructor() == TdApi.ChatListFolder.CONSTRUCTOR)
-            .map(folder -> ((TdApi.ChatListFolder) folder).chatFolderId)
-            .collect(Collectors.toSet());
   }
 }
